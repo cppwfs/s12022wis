6d3b647ca8171a5ecc933b5b261c2acd4322345a
Glenn Renfro <grenfro@vmware.com>
2022-10-28 17:43:23 -0400

    Introduction to source control

---
+++ src/main/java/io/spring/whatisspring/WhatisspringApplication.java
@@ -0,0 +1,13 @@
+package io.spring.whatisspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatisspringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WhatisspringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/io/spring/whatisspring/WhatisspringApplicationTests.java
@@ -0,0 +1,13 @@
+package io.spring.whatisspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WhatisspringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
